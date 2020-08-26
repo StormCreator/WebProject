cf1c86353daa7d88e6ab0b6f5d554d80ebeb2151
MarginToppson <insetml@gmail.com>
2020-08-26 18:49:54 +0300

    another change

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
-
+import WorkCards from './modules/workCards';
 
 window.addEventListener('DOMContentLoaded', function() {
+
+    WorkCards('.weDo__descr-arrow img', '.weDo__descr-content');
 });
 
 
